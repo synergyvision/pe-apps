0a8281bc1a5ad0c0e949ec6c74c5af8a6d9b646e
YoselinArvelaiz <ya@synergy.vision>
2018-10-01 15:36:42 -0400

    Algunos cambios.

--- 005-Diagrama-de-tallo-y-hoja/app.R
+++ 005-Diagrama-de-tallo-y-hoja/app.R
@@ -14,8 +14,7 @@ ui <- fluidPage(
                   min = 0.2,
                   max = 2,
                   value = 0.2,
-                  step = 0.1),
-      verbatimTextOutput("prueba")
+                  step = 0.1)
       ),
     
     
@@ -31,14 +30,11 @@ server <- function(input, output) {
 
   sueldos <- c(47,47,47,47,48,49,50,50,50,51,51,51,51,52,52,52,52,52,52,54,54,
                54,54,54,57,60,49,49,50,50,51,51,51,51,52,52,56,56,57,57,52,52)
-  
-  output$prueba<-renderPrint({
-    input$scale
-  })
+
   
   output$displot <- renderPrint({
        
-  stem(x=sueldos, scale = input$scale)
+  stem(x=sueldos, scale = as.numeric(input$scale))
     
 
     })
