b6a240dfa9d92ca1315fd0cf0a7cb8dff6fbdc21
JoseCastillo-vision <jc@synergy.vision>
2018-10-03 13:23:55 -0400

    actualizando la parte de cargar datos

--- 002-Histogramas/app.R
+++ 002-Histogramas/app.R
@@ -19,19 +19,18 @@ ui <- fluidPage(
       conditionalPanel( condition = "input.n=='Ejemplos del libro'",
                         selectInput( inputId = "m", 
                                      label = "Ejemplo",
-                                     choices= c('Sueldos','ventas','Otros'), 
+                                     choices= c('Sueldos','Ventas','Otros'), 
                                      selected = NULL),
-                        conditionalPanel(condition = "input.m=='Sueldos'",
-                                         selectInput( inputId = "n1", 
-                                                      label = "Ejemplo",
-                                                      choices= c('Frecuencia absoluta','Frecuencia relativa'), 
-                                                      selected = NULL),
-                                         sliderInput(inputId = "bins",
-                                                     label = "Número de intervalos:",
-                                                     min = 1,
-                                                     max = 10,
-                                                     value = 1)
-                                         )
+                        selectInput( inputId = "n1", 
+                                     label = "Ejemplo",
+                                     choices= c('Frecuencia absoluta','Frecuencia relativa'), 
+                                     selected = NULL),
+                        sliderInput(inputId = "bins",
+                                    label = "Número de intervalos:",
+                                     min = 1,
+                                     max = 10,
+                                     value = 1)
+                                         
                         ),
       conditionalPanel( condition = "input.n=='cargados'",
                         fileInput( inputId = "datoscargados",
@@ -54,7 +53,8 @@ ui <- fluidPage(
 
     # Main panel for displaying outputs ----
     mainPanel(
-         plotOutput(outputId = "distPlot")
+         #plotOutput(outputId = "distPlot")
+         tableOutput("table")
     )
   )
 )
@@ -62,41 +62,81 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
-  sueldos <- c(47,47,47,47,48,49,50,50,50,51,51,51,51,52,52,52,52,52,52,54,54,
+  Sueldos <- c(47,47,47,47,48,49,50,50,50,51,51,51,51,52,52,52,52,52,52,54,54,
                54,54,54,57,60,49,49,50,50,51,51,51,51,52,52,56,56,57,57,52,52)
-  dat<-data.frame(sueldos)
   
-  output$distPlot <- renderPlot({
+  Ventas<-c(rep(1,4),rep(2,5),rep(3,2),rep(4,10),rep(5,9),rep(6,6),rep(7,6))
+  
+  Otros<-c(rep(10,4),rep(22,5),rep(35,2),rep(46,10),rep(57,9),rep(68,6),rep(74,6))
+  
+  dat<-reactive({
     
-    if(input$n=='Frecuencia absoluta'){
+    infile <- input$n
+    if(is.null(infile)){
+      return()
+      }
     
-    ggplot(dat,aes(x=sueldos))+
-      geom_histogram( aes(y=..count..),
-                      closed="left",bins = input$bins,
-                      fill="blue",col="black",alpha=0.7)+
-      labs(title = "Histograma", x="Clases", y="Frecuencia")
+    else if(infile=='Ejemplos del libro'){
       
+      infile1<-input$m
+      
+      if(infile1=='Sueldos'){
+      data.frame(Sueldos)
+      }
+      
+      else if(infile1=='Ventas'){
+        data.frame(Ventas)
+      }
+      
+      else if(infile1=='Otros')
+        data.frame(Otros)
     }
     
-    else if(input$n=='Frecuencia relativa'){
-      
-      ggplot(dat,aes(x=sueldos))+
-        geom_histogram( aes(y=..density..),
-                        closed="left",bins = input$bins,
-                        fill="blue",col="black",alpha=0.7)+
-        labs(title = "Histograma", x="Clases", y="Frecuencia Relativa")
-      
+    else if(infile=='cargados'){
+      return()
     }
-
-
+    
+    else if(infile=='Generados aleatoriamente'){
+      return()
+    }
+    
     })
+  
+  output$table<-renderTable({
+    return(data.frame(dat()))
+  })
+  
+  # output$distPlot <- renderPlot({
+  #   
+  #   if(input$n=='Frecuencia absoluta'){
+  #   
+  #   ggplot(dat,aes(x=sueldos))+
+  #     geom_histogram( aes(y=..count..),
+  #                     closed="left",bins = input$bins,
+  #                     fill="blue",col="black",alpha=0.7)+
+  #     labs(title = "Histograma", x="Clases", y="Frecuencia")
+  #     
+  #   }
+  #   
+  #   else if(input$n=='Frecuencia relativa'){
+  #     
+  #     ggplot(dat,aes(x=sueldos))+
+  #       geom_histogram( aes(y=..density..),
+  #                       closed="left",bins = input$bins,
+  #                       fill="blue",col="black",alpha=0.7)+
+  #       labs(title = "Histograma", x="Clases", y="Frecuencia Relativa")
+  #     
+  #   }
+  # 
+  # 
+  #   })
 
 }
 
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
 
-
+#----------------------------------------------------------------------------------------------------
 # selectInput( inputId = "n", 
 #              label = "Tipo de frecuencia",
 #              choices= c('Frecuencia absoluta','Frecuencia relativa'), 
