bd82d32fb6a3745f8af9769127224933ddc66e70
YoselinArvelaiz <ya@synergy.vision>
2018-10-04 13:51:08 -0400

    Parte de los cálculos de la medidas de tendencia central de la aplicación.

--- 007-Medidas-de-tendencia-central/app.R
+++ 007-Medidas-de-tendencia-central/app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(readxl)
+library(psych)
+library(modeest)
 
 ui <- fluidPage(
   
@@ -16,10 +18,10 @@ ui <- fluidPage(
       conditionalPanel( condition = "input.n == 'gen'", 
                          sliderInput( "m",
                                      label = "Número de filas:",
-                                     min = 1, max = 20, value = 1, step = 10),
+                                     min = 1, max = 20, value = 5),
                         sliderInput( "f",
                                      label = "Número de variables:",
-                                     min = 1, max = 10, value = 1, step = 5)
+                                     min = 1, max = 10, value = 5)
        ),
       conditionalPanel( condition = "input.n == 'car'", 
                         fileInput(inputId = "datoscargados",label = "Seleccionar desde un archivo guardado:", 
@@ -28,13 +30,19 @@ ui <- fluidPage(
       
       conditionalPanel( condition = "input.n == 'ejem'", 
                         selectInput( inputId = "ejemplos", label = "Datos de ejemplo:",
-                                     choices= c("Sueldos","Ventas","Datos"), 
+                                     choices= c("Sueldos","Horas","Ventas"), 
                                      selected = NULL)
-      )
+      ),
+      
+      selectInput( inputId = "medias", label = "Medidas de Tendencia Central:",
+                   choices= c("Media Aritmética","Media Geométrica","Media Armónica",
+                              "Media Ponderada","Mediana","Moda"), 
+                   selected = NULL)
       
     ),
     mainPanel(
-       tableOutput("table")
+       tableOutput("table"),
+       verbatimTextOutput("medias")
     )
   )
 )
@@ -62,17 +70,48 @@ server <- function(input, output) {
        return()
      }
      
-     D<-read_excel(file1$datapath)
-     data.frame(D)
+     read_excel(file1$datapath)
+     
       
-    }
+   } else if(input$n=="ejem"){
+     
+     Sueldos<- c(47,47,47,47,48,49,50,50,50,51,51,51,51,52,52,52,52,52,52,54,54,
+                      54,54,54,57,60,49,49,50,50,51,51,51,51,52,52,56,56,57,57,52,52)
+     Horas<-c(rep(2,46),rep(3,15),rep(4,12),rep(6,52),rep(7,8))
+     
+     Ventas <- c(1034,1075,1123,1172,1218,1265,1313,1379,1452,1597)
+     
+        if(input$ejemplos=="Sueldos"){
+          data.frame(Sueldos)
+        } else if(input$ejemplos=="Horas"){
+          data.frame(Horas)
+        } else if(input$ejemplos=="Ventas"){
+             data.frame(Ventas)
+        }
+      
+  }
  
   })
   
  output$table <- renderTable({ data() },
                               striped = TRUE,hover = TRUE,
-                              bordered = TRUE,rownames = TRUE
- )
+                              bordered = TRUE,rownames = FALSE)
+ output$medias<-renderPrint({
+   
+   if(input$medias=="Media Aritmética"){
+     colMeans(data())
+   } else if(input$medias=="Media Geométrica"){
+     geometric.mean(data())
+   } else if(input$medias=="Media Armónica"){
+     harmonic.mean(data())
+   } else if(input$medias=="Mediana"){
+     apply(data(),2,median)
+   } else if(input$medias=="Moda"){
+     apply(data(),2,mfv)
+   }
+   
+ })
+                             
  
 }
 
