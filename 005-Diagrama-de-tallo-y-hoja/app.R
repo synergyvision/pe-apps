bf14f805e39f67bb04758f9c028cd8fafb954fc2
YoselinArvelaiz <ya@synergy.vision>
2018-11-05 17:15:44 -0400

    Interfaz app Diagrama de tallo y hoja

--- 005-Diagrama-de-tallo-y-hoja/app.R
+++ 005-Diagrama-de-tallo-y-hoja/app.R
@@ -8,19 +8,51 @@ ui <- fluidPage(
   sidebarLayout(
 
     sidebarPanel(
-
-      sliderInput(inputId = "scale",
-                  label = "Número de escala:",
-                  min = 0.2,
-                  max = 2,
-                  value = 0.2,
-                  step = 0.1)
+      
+      radioButtons( inputId = "n",label = "Origen de los datos:", 
+                    choices = c("Generados"="gen", "Cargados"="car", "Ejemplos"="ejem"),
+                    selected=" "),
+      conditionalPanel( condition = "input.n == 'gen'", 
+                        sliderInput( "m",
+                                     label = "Cantidad de datos:",
+                                     min = 1, max = 50, value = 5),
+                        sliderInput(inputId = "scale",
+                       label = "Número de escala:",
+                       min = 0.2,
+                       max = 2,
+                       value = 0.2,
+                       step = 0.1)),
+      conditionalPanel( condition = "input.n == 'car'", 
+                        fileInput(inputId = "datoscargados",label = "Seleccionar desde un archivo guardado:", 
+                                  buttonLabel = "Buscar...", placeholder = "Aun no seleccionas el archivo..."),
+                        numericInput( inputId = "d", label="Escoja el número de columna deseado:", min = 1,
+                                      max = 100,step = 1,
+                                      value = 1, width = "40%"),
+                        sliderInput(inputId = "scale1",
+                                    label = "Número de escala:",
+                                    min = 0.2,
+                                    max = 2,
+                                    value = 0.2,
+                                    step = 0.1)),
+      conditionalPanel( condition = "input.n == 'ejem'", 
+                        selectInput( inputId = "ejemplos", label = "Datos de ejemplo:",
+                                     choices= c("Sueldos","Horas","Ventas"), 
+                                     selected = NULL),
+                        sliderInput(inputId = "scale1",
+                                    label = "Número de escala:",
+                                    min = 0.2,
+                                    max = 2,
+                                    value = 0.2,
+                                    step = 0.1))
+                        
+                        
       ),
     
     
        mainPanel(
          
-              verbatimTextOutput("displot")
+         column(width=4,div(style="height:400px; overflow-y: scroll",tableOutput(outputId = "table"))),
+              column(width=8,verbatimTextOutput("displot"))
     )
   )
 )
