d7272d1264940b392f08c3f2675a4829d2197ebc
YoselinArvelaiz <ya@synergy.vision>
2018-11-23 16:56:26 -0400

    parte del server de la aplicación de conjuntos

--- 011-Teoría-de-Conjuntos/app.R
+++ 011-Teoría-de-Conjuntos/app.R
@@ -10,6 +10,7 @@ ensure_version("psych", "1.8.4")
 ensure_version("modeest", "2.1")
 ensure_version("matrixStats", "0.54.0")
 ensure_version("ggplot2","3.0.0")
+ensure_version("sets","1.0-18")
 
 library(shiny)
 library(shinydashboard)
@@ -18,6 +19,7 @@ library(psych)
 library(modeest)
 library(matrixStats)
 library(ggplot2)
+library(sets)
 
 ui <- fluidPage(
   
@@ -32,17 +34,16 @@ ui <- fluidPage(
       textInput(inputId = "con2", label = "Introducir el segundo conjunto",placeholder = "a,b,..."),
       textInput(inputId = "con3", label = "Introducir el tercer conjunto",placeholder = "a,b,..."),
       
-      conditionalPanel(condition = "input.con3", checkboxGroupInput(inputId = "con",label="Selección de Conjuntos",
-                                                                    choices = c("Conjunto 1","Conjunto 2","Conjunto 3",selected=NULL))),
-      conditionalPanel(condition="input.con",selectInput(inputId = "ope",label = "Operaciones de cojuntos",choices = c("Unión","Intersección","Diferencia","Complemento","Producto Cartesiano","Potencia"),
-                                                         selected = NULL))
-    ),
+      
+      conditionalPanel(condition="input.con1",selectInput(inputId = "ope",label = "Operaciones de cojuntos",choices = c("Unión","Intersección","Diferencia","Complemento","Producto Cartesiano","Potencia"),
+                                                         selected = NULL),checkboxGroupInput(inputId = "con",label="Selección de Conjuntos",
+                                                                   choices = c("Conjunto 1"= "con1","Conjunto 2"= "con2","Conjunto 3"= "con3",selected=NULL)))
+      ),
+
     mainPanel(
       
-      column(width=5,h3("Datos"),div(style="height:400px; overflow-y: scroll",tableOutput("table"))),
-      
-      conditionalPanel(condition ="input.medias=='Cuantiles'",column(width=7,h3("Cuantiles"),verbatimTextOutput("cuantiles"))),
-      conditionalPanel(condition ="input.medias=='Diagrama de Caja'",column(width=7,h3("Diagrama de Caja"),plotOutput("plot")))
+      column(width=5,h3("Operación"),verbatimTextOutput("op1")),
+      column(width=5,h3("Diagrama de Venn"),plotOutput("plot"))
     )
   )
 )
@@ -50,114 +51,46 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-
- data<-reactive ({
-   if (is.null(input$n)){
-     return()
-   }
-   
-   else if(input$n=="gen"){
-    M<-matrix( as.integer(runif(input$m*input$f,1,20)),
-               ncol = input$f, nrow = input$m)
-    colnames(M)<-c(paste0("Vari_",1:input$f))
-    rownames(M)<-c(paste0("fil_",1:input$m))
-    return(M)
-   } else if(input$n=="car"){
-     
-     file1<-input$datoscargados
-     
-     if(is.null(file1)){
-       return()
-     }
-     
-    read_excel(file1$datapath)
-    
-      
-   } else if(input$n=="ejem"){
-     
-     Sueldos<- c(47,47,47,47,48,49,50,50,50,51,51,51,51,52,52,52,52,52,52,54,54,
-                      54,54,54,57,60,49,49,50,50,51,51,51,51,52,52,56,56,57,57,52,52)
-     Horas<-c(rep(2,46),rep(3,15),rep(4,12),rep(6,52),rep(7,8))
-     
-     Ventas <- c(1034,1075,1123,1172,1218,1265,1313,1379,1452,1597)
-     
-        if(input$ejemplos=="Sueldos"){
-          data.frame(Sueldos)
-        } else if(input$ejemplos=="Horas"){
-          data.frame(Horas)
-        } else if(input$ejemplos=="Ventas"){
-             data.frame(Ventas)
-        }
-      
-  }
- 
-  }) 
- 
- 
- output$table <- renderTable({ data() },
-                              striped = TRUE,hover = TRUE,
-                              bordered = TRUE,rownames = FALSE)
- 
- 
- 
- output$cuantiles<-renderPrint({
-   
-   if(is.null(input$medias)||is.null(data())){
-     return()
-   }
   
-   if(input$medias=="Cuantiles"){
-      if(is.null(input$vect)){
-        return()
-       } else{
-       x <- as.numeric(unlist(strsplit(input$vect,",")))
-    
-       apply(data(),2,quantile,probs = x)
-       
-       }
-     
-   }
+  conjunto1<-reactive({
+    if(is.null(input$con1)){
+      return(NULL)
+    } else{
+      as.vector(unlist(strsplit(input$con1,",")))
+    }
+  })
+  
+   conjunto2<-reactive({ 
+    if(is.null(input$con2)){
+      return(NULL)
+    } else{
+      as.vector(unlist(strsplit(input$con2,",")))
+    }
 })
+    
+   conjunto3<-reactive({
+    if(is.null(input$con3)){
+      return(NULL)
+    } else{
+      as.vector(unlist(strsplit(input$con3,",")))
+    }
+   })   
    
-   output$plot<-renderPlot({
-     
-     if(input$medias=="Diagrama de Caja"){
-       
-       if(input$n=="gen"){
-         datos<-as.data.frame(data())
-         
-         ncol<-input$k
-         
-         ggplot(datos, aes(x=length(datos[,ncol]),y=datos[,ncol]))+
-           geom_boxplot(fill="blue",alpha=0.5)+
-           labs(title = "Diagrama de Caja",
-                x=colnames(datos[ncol]),y="")
-       }
-       
-       else if(input$n=="car"){
-       datos<-as.data.frame(data())
-       
-       ncol<-input$d
-       
-       ggplot(datos, aes(x=length(datos[,ncol]),y=datos[,ncol]))+
-         geom_boxplot(fill="blue",alpha=0.5)+
-         labs(title = "Diagrama de Caja",
-              x=colnames(datos[ncol]),y="")
-       }
-       else if(input$n=="ejem"){
-         datos<-as.data.frame(data())
+   
+d<-reactive({
+  rbind(conjunto1(),conjunto2(),conjunto3())
+  
+})
 
-         ggplot(datos, aes(x=length(datos[,1]),y=datos[,1]))+
-           geom_boxplot(fill="blue",alpha=0.5)+
-           labs(title = "Diagrama de Caja",
-                x=colnames(datos[1]),y="")
-       }
-       
-     }  
-     
-     
-   })
-                             
+
+
+ output$op1<-renderPrint({
+   
+   if(input$con=="con1"){
+     print(d()[1,])
+   } 
+   
+ })
  
 }
 
