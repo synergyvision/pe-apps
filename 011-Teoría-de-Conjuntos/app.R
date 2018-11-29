fe99db5b3b3d31d84fa42157d32051f78a960f5b
YoselinArvelaiz <ya@synergy.vision>
2018-11-29 15:12:03 -0400

    Parte del server de la app de conjuntos

--- 011-Teoría-de-Conjuntos/app.R
+++ 011-Teoría-de-Conjuntos/app.R
@@ -11,6 +11,7 @@ ensure_version("modeest", "2.1")
 ensure_version("matrixStats", "0.54.0")
 ensure_version("ggplot2","3.0.0")
 ensure_version("sets","1.0-18")
+ensure_version("RAM","1.2.1.7")
 
 library(shiny)
 library(shinydashboard)
@@ -20,6 +21,8 @@ library(modeest)
 library(matrixStats)
 library(ggplot2)
 library(sets)
+library(RAM)
+
 
 ui <- fluidPage(
   
@@ -32,12 +35,11 @@ ui <- fluidPage(
       
       textInput(inputId = "con1", label = "Introducir el primer conjunto",placeholder = "a,b,..."),
       textInput(inputId = "con2", label = "Introducir el segundo conjunto",placeholder = "a,b,..."),
-      textInput(inputId = "con3", label = "Introducir el tercer conjunto",placeholder = "a,b,..."),
       
       
       conditionalPanel(condition="input.con1",selectInput(inputId = "ope",label = "Operaciones de cojuntos",choices = c("Unión","Intersección","Diferencia","Complemento","Producto Cartesiano","Potencia"),
                                                          selected = NULL),checkboxGroupInput(inputId = "con",label="Selección de Conjuntos",
-                                                                   choices = c("Conjunto 1"="con1","Conjunto 2"="con2","Conjunto 3"="con3",selected=NULL)))
+                                                                   choices = c("Conjunto 1"="con1","Conjunto 2"="con2",selected=NULL)))
       
       ),
 
@@ -72,32 +74,91 @@ server <- function(input, output,session) {
      }
  })
  
-     conjunto3<-reactive({
-       if(is.null(input$con=="con3")){
-       return(NULL)
-      } else{
-         as.vector(unlist(strsplit(input$con3,",")))
-       }
-      })
-     
       
-   d<-reactive({
-    w<-rbind(conjunto1(),conjunto2(),conjunto3())
-     row.names(w)<-c("con1","con2","con3")
-     return(w)
-     
-   })
+    d<-reactive({
+     w<-rbind(conjunto1(),conjunto2())
+      row.names(w)<-c("con1","con2")
+      return(w)
+      
+    })
 
 
  output$op1<-renderPrint({
    
-   if(is.null(input$con)){
-     return(NULL)
-   } else{
-     #d()[c(input$con),]
-     union(d()[c(input$con),],d()[c(input$con),])
-   }
+    if(is.null(input$con)){
+      return(NULL)
+    } else{
+      if(input$ope=="Unión"){
+        if(length(input$con)==1){
+          union(d()[input$con[1],],d()[input$con[1],])
+        } else{
+      union(d()[input$con[1],],d()[input$con[2],])
+        }
+      } else if(input$ope=="Intersección"){
+        if(length(input$con)==1){
+          intersect(d()[input$con[1],],d()[input$con[1],])
+        } else{
+          intersect(d()[input$con[1],],d()[input$con[2],])
+        }
+      }
+        else if(input$ope=="Diferencia"){
+          if(length(input$con)==1){
+            setdiff(d()[input$con[1],],d()[input$con[1],])
+          } else{
+            #Diferencia Conjunto 1 - Conjunto 2
+           setdiff(d()[input$con[1],],d()[input$con[2],])
+          }}
+        else if(input$ope=="Producto Cartesiano"){
+            
+            cartesian <- function(a, b) {
+              axb <- list()
+              k <- 1
+              for (i in a) {
+                for (j in b) {
+                  axb[[k]] <- c(i,j)
+                  k <- k + 1
+                }
+              }
+              return(axb)
+            }
+            
+            if(length(input$con)==1){
+              cartesian(d()[input$con[1],],d()[input$con[1],])
+            } else{
+              cartesian(d()[input$con[1],],d()[input$con[2],])
+              }
+        }
+       
+     }
    
+})
+ 
+output$plot<-renderPlot({
+  if(is.null(input$con)){
+    return(NULL)
+  }
+  else if(input$ope=="Unión"){
+    a<-d()[input$con[1],]
+    b<-d()[input$con[2],]
+    group.venn(list(Conjunto1=a, Conjunto2=b), label=TRUE, 
+               fill = c("blue", "blue"),
+               cat.pos = c(0, 0),
+               lab.cex=1.1)
+  } else if(input$ope=="Intersección"){
+    a1<-d()[input$con[1],]
+    b1<-d()[input$con[2],]
+    group.venn(list(Conjunto1=a1, Conjunto2=b1), label=TRUE, 
+               fill = c("green", "blue"),
+               cat.pos = c(0, 0),
+               lab.cex=1.1)
+  } else if(input$ope=="Diferencia"){
+    a1<-d()[input$con[1],]
+    b1<-d()[input$con[2],]
+    group.venn(list(Conjunto1=a1, Conjunto2=b1), label=TRUE, 
+               fill = c("green", "white"),
+               cat.pos = c(0, 0),
+               lab.cex=1.1)
+  }
 })
 
 }
