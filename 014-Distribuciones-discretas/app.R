e52764360b0971acfb4fd2ac9e86e46c73efb10a
JoseCastillo-vision <jc@synergy.vision>
2019-01-02 15:54:44 -0400

    actualizacion caracteristicas binomial y geometrica

--- 014-Distribuciones-discretas/app.R
+++ 014-Distribuciones-discretas/app.R
@@ -23,8 +23,6 @@ library(matrixStats)
 library(ggplot2)
 
 
-
-
 ui <- fluidPage(
   
   titlePanel("Distribuciones discretas"),
@@ -39,11 +37,8 @@ ui <- fluidPage(
 
       withMathJax(),
        conditionalPanel(condition = "input.distribucion=='Bernoulli'",includeMarkdown("bernoulli.Rmd")),
-      conditionalPanel(condition = "input.distribucion=='Binomial'",fluidRow(h1(HTML("$$\\alpha^2$$"))),
-                       h3('Funcion de densidad'),fluidRow(h1(HTML("$$f(x)=x$$")))),
-      conditionalPanel(condition = "input.distribucion=='Geométrica'",h3('Función de Densidad'),fluidRow(h1(HTML("$$f(x) =(1-p)^{x-1} p$$"))),
-                       h3('Función de Distribución'),fluidRow(h1(HTML("$$F(x) = 1- (1-p)^{x+1}$$"))),
-                       h3('Media'),fluidRow(h1(HTML("$$\\mathbb{E[x]}=\\displaystyle \\frac{1}{p}$$")))),
+      conditionalPanel(condition = "input.distribucion=='Binomial'",includeMarkdown("binomial.Rmd")),
+      conditionalPanel(condition = "input.distribucion=='Geométrica'",includeMarkdown("geometrica.Rmd")),
       conditionalPanel(condition = "input.distribucion=='Hipergeométrica'",includeMarkdown("hipergeometrica.Rmd")),
       conditionalPanel(condition = "input.distribucion=='Multinomial'",includeMarkdown("multinomial.Rmd")),
       conditionalPanel(condition = "input.distribucion=='Poisson'",h1(HTML("$$\\alpha+\\beta$$"))),
