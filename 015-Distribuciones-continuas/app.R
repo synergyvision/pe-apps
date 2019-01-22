88c935f28d0ca7c5e5ae0bbf66885ae6ee5e19ca
JoseCastillo-vision <jc@synergy.vision>
2019-01-22 16:12:26 -0400

    densidad uniforme

--- 015-Distribuciones-continuas/app.R
+++ 015-Distribuciones-continuas/app.R
@@ -39,7 +39,13 @@ ui <- fluidPage(
 
       withMathJax(),
       conditionalPanel(condition = "input.distribucion=='Uniforme'",tabsetPanel(type = "pills", id="pri",tabPanel('Características',includeHTML("uniforme.html")),
-                                                                                 tabPanel('Cálculos',br(),br()))),
+                                                                                 tabPanel('Cálculos',br(),br(),column(width=5,selectInput(inputId = 'uni',label = HTML('Seleccione el cálculo deseado'),choices = c('Función de Densidad','Función de Distribución','Cuantiles','Muestra Aleatoria'),selected = NULL),
+                                                                                          conditionalPanel(condition = "input.uni=='Función de Densidad'",
+                                                                                                           sliderInput('rangouni',label = 'Parámetros [a,b]',min = -40,max=40,value=c(0,1),step = 0.1,width = '300px'),
+                                                                                                           numericInput(inputId = 'valor',label = HTML('Seleccione el valor de la función de densidad'),min=-40,max=40,step=0.1,value = 0.5,width = '150px'))
+                                                                                          ),
+                                                                                          conditionalPanel(condition = "input.uni=='Función de Densidad'",column(align='center',width=7,br(),verbatimTextOutput("unif"),plotOutput("dens1")))
+                                                                                          ))),
       conditionalPanel(condition = "input.distribucion=='Exponencial'",tabsetPanel(type = "pills", id="pri2",tabPanel("Características",includeHTML("exponencial.html")),
                                                                                 tabPanel('Cálculos',br(),br()))),
       conditionalPanel(condition = "input.distribucion=='Gamma'",tabsetPanel(type = "pills", id="pri3",tabPanel("Características",includeHTML("gamma.html")),
@@ -63,6 +69,36 @@ ui <- fluidPage(
 
 
 server <- function(input, output,session) {
+  
+  output$unif<-renderText({
+    x<-input$valor
+    l<-input$rangouni[1]
+    u<-input$rangouni[2]
+    resultado<-paste("f(",x,") = ", dunif(x,l,u))
+    return(resultado)
+  })
+  
+  output$dens1<-renderPlot({
+    x1<-input$valor
+    l<-input$rangouni[1]
+    u<-input$rangouni[2]
+    
+    x <- seq(-40,40,0.01)
+    hx <- dunif(x,l,u)
+    
+    dat<-data.frame(x,hx)
+    
+    f<-ggplot(data=dat, mapping = aes(x,hx))+geom_line()+
+      geom_area(mapping = aes(x= ifelse(x >= l & x <= u,x,0)), fill = "blue",alpha = 0.4)+
+      geom_segment(aes(x = x1, y =0 , xend = x1,
+                       yend = dunif(x1,l,u)),
+                   colour = "black",linetype=2)+
+      xlim(-40,40)+
+      ylim(0,1.1)+
+      labs( title = 'Densidad uniforme',
+            x = "x", y = "f(x)",caption = "http://synergy.vision/" )
+    return(f)
+  })
 
   }
 
