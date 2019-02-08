5b30cc52a031cbcf51eb16e4aee3310f27d4c872
JoseCastillo-vision <jc@synergy.vision>
2019-02-08 13:27:35 -0400

    interfaz varianza conocida

--- 017-Pruebas-de-hipotesis/app.R
+++ 017-Pruebas-de-hipotesis/app.R
@@ -26,6 +26,10 @@ library(ggplot2)
 ui <- fluidPage(
   
   titlePanel("Pruebas de Hipotesis"),
+  sidebarLayout(
+    
+    sidebarPanel(
+  
   selectInput(inputId = 'ph',label = 'Escoja la Prueba de Hipótesis deseada',choices = c('Media de una población','Diferencia de medias de dos poblaciones',
                                                                                          'Varianza de una población','Igualdad de varianzas de dos poblaciones','Proporción en una población'),
               selected = NULL),
@@ -38,13 +42,60 @@ ui <- fluidPage(
   conditionalPanel(condition = "input.ph == 'Varianza de una población'", selectInput(inputId = 'tp2',label = 'Tipo de Prueba',choices = c('Dos colas','Cola superior','Cola inferior'),selected = NULL)),
   conditionalPanel(condition = "input.ph == 'Igualdad de varianzas de dos poblaciones'", selectInput(inputId = 'tp3',label = 'Tipo de Prueba',choices = c('Dos colas','Cola superior','Cola inferior'),selected = NULL)),
   conditionalPanel(condition = "input.ph == 'Proporción en una población'", selectInput(inputId = 'tp4',label = 'Tipo de Prueba',choices = c('Dos colas','Cola superior','Cola inferior'),selected = NULL))
+    ),
+  
+  mainPanel(
+    conditionalPanel(condition = "input.ph == 'Media de una población' & input.vc == 'Varianza conocida'",column(width=5,numericInput(inputId = 'MediaHip',label = 'Inserte Media hipotética',min=0,max = 100,value = 0,step = 0.1,width = '150px'),
+                     numericInput(inputId = 'MediaMuestral',label = 'Inserte Media de la muestra',min=0,max = 100,value = 5,step = 0.1,width = '150px'),
+                     numericInput(inputId = 'Muestra',label = 'Inserte Tamaño de la muestra',min=0,max = 100,value = 5,step = 1,width = '150px'),
+                     numericInput(inputId = 'VarianzaPob',label = 'Inserte Varianza Poblacional',min=0.1,max = 50,value = 1,step = 0.1,width = '150px'),
+                     numericInput(inputId = 'signif',label = 'Inserte Nivel de Significancia',min=0.01,max = 0.1,value = 0.05,step = 0.01,width = '150px')
+    ),column(width = 7,plotOutput('grafica1'))
+                     )
+    )
+  )
 )
 
 
 
 server <- function(input, output,session) {
   
-  
+  output$grafica1<-renderPlot({
+    mu<-input$MediaHip
+    x_bar<-input$MediaMuestral
+    n<-input$Muestra
+    sigma<-sqrt(input$VarianzaPob)
+    alpha<-input$signif
+    
+    z<-(x_bar-mu)*sqrt(n)/sigma
+    
+    z_alpha<-if(z<0){
+      qnorm(alpha,mean=0,sd=1)
+    }
+    else{
+      qnorm(1-alpha,mean=0,sd=1)
+    }
+    
+    x<-seq(-4,4,0.01)
+    y<-dnorm(x,mean=0, sd=1)
+    f<-ggplot(mapping = aes(x,y))+geom_line(colour = "blue")+
+      geom_area(mapping = aes(x,y), fill = "blue",alpha = .2)+
+      
+      geom_segment(aes(x = z_alpha, y =0 , xend = z_alpha, yend = dnorm(z_alpha,mean=0, sd=1)), colour = "black",linetype=2)+
+      geom_segment(aes(x = z, y =0 , xend = z, yend = dnorm(z,mean=0, sd=1)), colour = "red",linetype=1)+
+      
+      annotate("text", x=z, y =-0.02, label ="z", parse = TRUE)+
+      annotate("text", x=z_alpha, y =-0.02, label="'Z'[alpha]", parse = TRUE)+
+      annotate("text", x=0, y = 0.1, label="'Aceptar H'[0]", parse = TRUE)+
+      annotate("text", x=z_alpha+0.5, y=0.05, label="'Rechazar H'[0]", parse = TRUE)+
+      
+      ylim(-0.05,0.41)+
+      xlim(-4,4)+
+      labs( title = "Prueba de cola derecha Distribución Normal",
+            x = " ", y = " ",caption = "http://synergy.vision/" )
+    
+    return(f)
+  })
   
   
   
