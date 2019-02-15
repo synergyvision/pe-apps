8b3dcbdeb0de44bf801d58d0dca0f9f6a8b129df
JoseCastillo-vision <jc@synergy.vision>
2019-02-15 10:34:15 -0400

    diferencia de medias poblacionales con varianza conocida lista

--- 017-Pruebas-de-hipotesis/app.R
+++ 017-Pruebas-de-hipotesis/app.R
@@ -26,7 +26,7 @@ ui <- fluidPage(
   titlePanel("Pruebas de Hipotesis"),
   sidebarLayout(
 
-    sidebarPanel(
+    sidebarPanel(width = 3,
 
   selectInput(inputId = 'ph',label = 'Escoja la Prueba de Hipótesis deseada',choices = c('Media de una población','Diferencia de medias de dos poblaciones',
                                                                                          'Varianza de una población','Igualdad de varianzas de dos poblaciones','Proporción en una población'),
@@ -42,22 +42,33 @@ ui <- fluidPage(
   conditionalPanel(condition = "input.ph == 'Proporción en una población'", selectInput(inputId = 'tp4',label = 'Tipo de Prueba',choices = c('Dos colas','Cola superior','Cola inferior'),selected = NULL))
     ),
 
-  mainPanel(
-    conditionalPanel(condition = "input.ph == 'Media de una población' & input.vc == 'Varianza conocida'",column(width=4,numericInput(inputId = 'MediaHip',label = 'Inserte Media hipotética',min=0,max = 100,value = 0,step = 0.1,width = '150px'),
+  mainPanel(withMathJax(),width = 9,
+    conditionalPanel(condition = "input.ph == 'Media de una población' & input.vc == 'Varianza conocida'",column(width=3,numericInput(inputId = 'MediaHip',label = HTML('Inserte Media hipotética &mu;<sub>o</sub>'),min=0,max = 100,value = 0,step = 0.1,width = '150px'),
                      numericInput(inputId = 'MediaMuestral',label = 'Inserte Media de la muestra',min=0,max = 100,value = 5,step = 0.1,width = '150px'),
                      numericInput(inputId = 'Muestra',label = 'Inserte Tamaño de la muestra',min=0,max = 100,value = 5,step = 1,width = '150px'),
                      numericInput(inputId = 'VarianzaPob',label = 'Inserte Varianza Poblacional',min=0.1,max = 50,value = 1,step = 0.1,width = '150px'),
                      numericInput(inputId = 'signif',label = 'Inserte Nivel de Significancia',min=0.01,max = 0.1,value = 0.05,step = 0.01,width = '150px')
     ),column(width = 8,align='center',plotOutput('grafica1'))
                      ),
-    conditionalPanel(condition = "input.ph == 'Media de una población' & input.vc == 'Varianza desconocida'",column(width=4,numericInput(inputId = 'MediaHip1',label = 'Inserte Media hipotética',min=0,max = 100,value = 0,step = 0.1,width = '150px'),
+    conditionalPanel(condition = "input.ph == 'Media de una población' & input.vc == 'Varianza desconocida'",column(width=3,numericInput(inputId = 'MediaHip1',label = HTML('Inserte Media hipotética &mu;<sub>o</sub>'),min=0,max = 100,value = 0,step = 0.1,width = '150px'),
                                                                                                                  numericInput(inputId = 'MediaMuestral1',label = 'Inserte Media de la muestra',min=0,max = 100,value = 5,step = 0.1,width = '150px'),
                                                                                                                  numericInput(inputId = 'Muestra1',label = 'Inserte Tamaño de la muestra',min=0,max = 100,value = 5,step = 1,width = '150px'),
                                                                                                                  numericInput(inputId = 'Varianzamu1',label = 'Inserte Varianza muestral',min=0.1,max = 50,value = 1,step = 0.1,width = '150px'),
                                                                                                                 numericInput(inputId = 'signif1',label = 'Inserte Nivel de Significancia',min=0.01,max = 0.1,value = 0.05,step = 0.01,width = '150px')
     ),column(width = 8,align='center',plotOutput('grafica2'))),
 
-    conditionalPanel(condition = "input.ph == 'Diferencia de medias de dos poblaciones' & input.vc1 == 'Varianza conocida'",column(width = 4,numericInput(inputId = 'Media1Hip',label = HTML('Inserte Media &mu;<sub>x</sub>'),min=0,max = 100,value = 0,step = 0.1,width = '150px')))
+    conditionalPanel(condition = "input.ph == 'Diferencia de medias de dos poblaciones' & input.vc1 == 'Varianza conocida'",column(width = 2,numericInput(inputId = 'Media1Hip',label = HTML('Inserte Media &mu;<sub>x</sub>'),min=0,max = 100,value = 1,step = 0.1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Media2Hip',label = HTML('Inserte Media &mu;<sub>y</sub>'),min=0,max =100,value = 1,step = 0.1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Media1Muestral',label = HTML('Inserte Media de la muestra X&#772;'),min=0,max = 100,value = 5,step = 0.1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Media2Muestral',label = HTML('Inserte Media de la muestra Y&#772;'),min=0,max = 100,value = 7,step = 0.1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'signif2',label = 'Inserte Nivel de Significancia',min=0.01,max = 0.1,value = 0.05,step = 0.01,width = '150px')
+                                                                                                                                   ),
+                                                                                                                            column(width = 2,br(),numericInput(inputId = 'Muestra11',label = HTML('Inserte Tamaño de la muestra n<sub>x</sub>'),min=0,max = 100,value = 10,step = 1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Muestra12',label = HTML('Inserte Tamaño de la muestra n<sub>y</sub>'),min=0,max = 100,value = 15,step = 1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Varianza1Pob',label = HTML('Inserte Varianza Poblacional &sigma;<sup>2</sup><sub>x</sub>'),min=0.1,max = 50,value = 1,step = 0.1,width = '150px'),
+                                                                                                                                   numericInput(inputId = 'Varianza2Pob',label = HTML('Inserte Varianza Poblacional &sigma;<sup>2</sup><sub>y</sub>'),min=0.1,max = 50,value = 2,step = 0.1,width = '150px')
+                                                                                                                                   ),
+                                                                                                                            column(width = 8,align='center',plotOutput('grafica3')))
 
 
     )
@@ -261,6 +272,106 @@ server <- function(input, output,session) {
 
       return(f)
     }
+  })
+
+  output$grafica3<-renderPlot({
+
+    mux<-input$Media1Hip
+    muy<-input$Media2Hip
+    x_bar<-input$Media1Muestral
+    y_bar<-input$Media2Muestral
+    nx<-input$Muestra11
+    ny<-input$Muestra12
+    sigma2x<-input$Varianza1Pob
+    sigma2y<-input$Varianza2Pob
+    alpha<-input$signif2
+
+    z<-((x_bar - y_bar)-(mux-muy))/(sqrt((sigma2x/nx) + (sigma2y/ny)))
+
+    x<-if(-6<=z & z<=6){
+      seq(-6,6,0.01)
+    } else if(-6>z){
+      seq(z-1,6,0.01)
+    } else if(z>6){
+      seq(-6,z+1,0.01)
+    }
+
+    y<-dnorm(x,mean=0, sd=1)
+
+    if(input$tp11=='Dos colas'){
+
+      alpha_2<-alpha/2
+
+      z_alpha1<-qnorm(alpha_2,mean=0,sd=1)
+
+      z_alpha2<-qnorm(1-alpha_2,mean=0,sd=1)
+
+      f<-ggplot(mapping = aes(x,y))+geom_line(colour = "blue")+
+        geom_area(mapping = aes(x,y), fill = "blue",alpha = 0.2)+
+        geom_area(mapping = aes(x=ifelse(x>=z_alpha2,x,NA),y=ifelse(x>=z_alpha2,dnorm(x,mean=0, sd=1),NA)), fill = "blue",alpha = 0.4)+
+        geom_area(mapping = aes(x=ifelse(x<=z_alpha1,x,NA),y=ifelse(x<=z_alpha1,dnorm(x,mean=0, sd=1),NA)), fill = "blue",alpha = 0.4)+
+
+        geom_segment(aes(x = z_alpha1, y =0 , xend = z_alpha1, yend = dnorm(z_alpha1,mean=0, sd=1)), colour = "black",linetype=2)+
+        geom_segment(aes(x = z_alpha2, y =0 , xend = z_alpha2, yend = dnorm(z_alpha2,mean=0, sd=1)), colour = "black",linetype=2)+
+        geom_segment(aes(x = z, y =0 , xend = z, yend = dnorm(z,mean=0, sd=1)), colour = "red",linetype=1)+
+
+        annotate("text", x=z, y =-0.02, label ="Z", parse = TRUE)+
+        annotate("text", x=z_alpha1, y =-0.02, label="-'Z'[alpha/2]", parse = TRUE)+
+        annotate("text", x=z_alpha2, y =-0.02, label="'Z'[alpha/2]", parse = TRUE)+
+        annotate("text", x=0, y = 0.1, label="'Aceptar H'[0]", parse = TRUE)+
+        annotate("text", x=z_alpha1-2, y=dnorm(z_alpha1,mean=0, sd=1), label="'Rechazar H'[0]", parse = TRUE)+
+        annotate("text", x=z_alpha2+2, y=dnorm(z_alpha2,mean=0, sd=1), label="'Rechazar H'[0]", parse = TRUE)+
+
+        ylim(-0.05,0.41)+
+        labs( title = "Prueba de dos colas Distribución Normal",
+              x = " ", y = " ",caption = "http://synergy.vision/" )
+
+      return(f)
+    }
+    else if(input$tp11=='Cola superior'){
+
+      z_alpha2<-qnorm(1-alpha,mean=0,sd=1)
+
+      f<-ggplot(mapping = aes(x,y))+geom_line(colour = "blue")+
+        geom_area(mapping = aes(x,y), fill = "blue",alpha = 0.2)+
+        geom_area(mapping = aes(x=ifelse(x>=z_alpha2,x,NA),y=ifelse(x>=z_alpha2,dnorm(x,mean=0, sd=1),NA)), fill = "blue",alpha = 0.4)+
+
+        geom_segment(aes(x = z_alpha2, y =0 , xend = z_alpha2, yend = dnorm(z_alpha2,mean=0, sd=1)), colour = "black",linetype=2)+
+        geom_segment(aes(x = z, y =0 , xend = z, yend = dnorm(z,mean=0, sd=1)), colour = "red",linetype=1)+
+
+        annotate("text", x=z, y =-0.02, label ="Z", parse = TRUE)+
+        annotate("text", x=z_alpha2, y =-0.02, label="'Z'[alpha]", parse = TRUE)+
+        annotate("text", x=0, y = 0.1, label="'Aceptar H'[0]", parse = TRUE)+
+        annotate("text", x=z_alpha2+2, y=dnorm(z_alpha2,mean=0, sd=1), label="'Rechazar H'[0]", parse = TRUE)+
+
+        ylim(-0.05,0.41)+
+        labs( title = "Prueba de cola superior Distribución Normal",
+              x = " ", y = " ",caption = "http://synergy.vision/" )
+
+      return(f)
+    }
+    else if(input$tp11=='Cola inferior'){
+      z_alpha1<-qnorm(alpha,mean=0,sd=1)
+
+      f<-ggplot(mapping = aes(x,y))+geom_line(colour = "blue")+
+        geom_area(mapping = aes(x,y), fill = "blue",alpha = 0.2)+
+        geom_area(mapping = aes(x=ifelse(x<=z_alpha1,x,NA),y=ifelse(x<=z_alpha1,dnorm(x,mean=0, sd=1),NA)), fill = "blue",alpha = 0.4)+
+
+        geom_segment(aes(x = z_alpha1, y =0 , xend = z_alpha1, yend = dnorm(z_alpha1,mean=0, sd=1)), colour = "black",linetype=2)+
+        geom_segment(aes(x = z, y =0 , xend = z, yend = dnorm(z,mean=0, sd=1)), colour = "red",linetype=1)+
+
+        annotate("text", x=z, y =-0.02, label ="Z", parse = TRUE)+
+        annotate("text", x=z_alpha1, y =-0.02, label="-'Z'[alpha]", parse = TRUE)+
+        annotate("text", x=0, y = 0.1, label="'Aceptar H'[0]", parse = TRUE)+
+        annotate("text", x=z_alpha1-2, y=dnorm(z_alpha1,mean=0, sd=1), label="'Rechazar H'[0]", parse = TRUE)+
+
+        ylim(-0.05,0.41)+
+        labs( title = "Prueba de dos colas Distribución Normal",
+              x = " ", y = " ",caption = "http://synergy.vision/" )
+
+      return(f)
+    }
+
   })
 
 
